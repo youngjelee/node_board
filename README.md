435f116318e57615d0ff14af8180a1da0a978776
youngjelee <wpdud1221@naver.com>
2021-11-16 02:57:16 +0900

    1

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# MEN 주소록
-이 저장소의 코드는
-[http://a-mean-blog.com/ko/blog/Node-JS-첫걸음/주소록-만들기](http://a-mean-blog.com/ko/blog/Node-JS-첫걸음/주소록-만들기)에서 작성되었습니다.
-<br>
+#참고
+-- https://www.a-mean-blog.com/ko/blog/Node-JS-%EC%B2%AB%EA%B1%B8%EC%9D%8C/%EC%A3%BC%EC%86%8C%EB%A1%9D-%EB%A7%8C%EB%93%A4%EA%B8%B0/%EC%A3%BC%EC%86%8C%EB%A1%9D-Index-New-Create
+
+
+RDBMS와 mongoDB의 비교 (비슷)
+
+table -> collection
+
+row -> document
+
+column -> field
+
+pk -> object id
+
+relation ship -> embedded linking
+
--- index.js
+++ index.js
@@ -1,32 +1,48 @@
+// index.js
+
 var express = require('express');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var bodyParser = require('body-parser'); // 1
 var app = express();
 
-// DB setting
-mongoose.connect(process.env.MONGO_DB);
-var db = mongoose.connection;
-db.once('open', function(){
-  console.log('DB connected');
-});
-db.on('error', function(err){
-  console.log('DB ERROR : ', err);
-});
+// DB setting ...
 
 // Other settings
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(methodOverride('_method'))
+app.use(bodyParser.json()); // 2
+app.use(bodyParser.urlencoded({extended:true})); // 3
+
+// DB schema // 4
+var contactSchema = mongoose.Schema({
+  name:{type:String, required:true, unique:true},
+  email:{type:String},
+  phone:{type:String}
+});
+var Contact = mongoose.model('contact', contactSchema); // 5
 
 // Routes
-app.use('/', require('./routes/home'));
-app.use('/contacts', require('./routes/contacts'));
-
-// Port setting
-var port = 3000;
-app.listen(port, function(){
-  console.log('server on! http://localhost:'+port);
+// Home // 6
+app.get('/', function(req, res){
+  res.redirect('/contacts');
 });
+// Contacts - Index // 7
+app.get('/contacts', function(req, res){
+  Contact.find({}, function(err, contacts){
+    if(err) return res.json(err);
+    res.render('contacts/index', {contacts:contacts});
+  });
+});
+// Contacts - New // 8
+app.get('/contacts/new', function(req, res){
+  res.render('contacts/new');
+});
+// Contacts - create // 9
+app.post('/contacts', function(req, res){
+  Contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
+
+// Port setting ...
